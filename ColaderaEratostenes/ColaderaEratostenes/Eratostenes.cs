6243ae5d1fe314a24632de5f453fed5279454780
MaricruzCF <mmichel5@ucol.mx>
2018-03-24 00:02:26 -0600

    Add files via upload

---
+++ ColaderaEratostenes/ColaderaEratostenes/Eratostenes.cs
@@ -0,0 +1,37 @@
+﻿namespace ColaderaEratostenes
+{
+    class Eratostenes
+    {
+        public static string NumerosPrimos(int limite)
+        {
+            string num = "";
+            int[] numeros = new int[limite];
+
+            for (int i = 0; i < limite; i++)
+                numeros[i] = 1;
+
+            for (int primo = 2; primo < limite; primo++)
+            {
+                for (int i = primo + 1; i < limite; i++)
+                {
+                    if (numeros[i] == 1)
+                    {
+                        if (i % primo == 0)
+                        {
+                            numeros[i] = 0;
+                        }
+                    }
+                }
+            }
+            for (int i = 1; i < limite; i++ )
+            {
+                if (numeros[i] == 1)
+                {
+                    num += i + ", ";
+                }
+            }
+
+            return num;
+        }
+    }
+}
